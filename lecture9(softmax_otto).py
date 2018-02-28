effb7ecfd45d344720e8b9b19c4ab368cc43c14b
eunho <lucky7323@postech.ac.kr>
2018-02-28 23:13:16 +0900

    change tensor type (cross-entropy requirements)

--- lecture9(softmax_otto).py
+++ lecture9(softmax_otto).py
@@ -2,7 +2,11 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+batch_size = 128
 
 def preprocessing():
     df = pd.read_csv('./data/otto/train.csv')
@@ -13,9 +17,11 @@ def preprocessing():
 class Dataset(Dataset):
     def __init__(self):
         preprocessing()
-        xy = np.loadtxt('./data/otto/train_processed.csv', delimiter=',', skiprows=1)
-        self.x_data = torch.from_numpy(xy[:, 0:-1])
-        self.y_data = torch.from_numpy(xy[:, [-1]])
+        xy = np.loadtxt('./data/otto/train_processed.csv',
+                        delimiter=',', skiprows=1, dtype=np.float32)
+        self.x_data = torch.from_numpy(xy[:, 1:-1])
+        y = torch.from_numpy(xy[:, [-1]])
+        self.y_data = y.type(torch.LongTensor)
         self.len = len(xy)
 
     def __getitem__(self, idx):
@@ -25,3 +31,38 @@ class Dataset(Dataset):
         return self.len
 
 dataset = Dataset()
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.l1 = torch.nn.Linear(93, 50)
+        self.l2 = torch.nn.Linear(50, 20)
+        self.l3 = torch.nn.Linear(20, 9)
+
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        return self.l3(x)
+
+model = Model()
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        pred = model(data)
+        loss = criterion(pred, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.4f}".format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+for epoch in range(1, 11):
+    train(epoch)
